5d693433a93182c0781f6ce42b30c6308d56bafb
Vinod M P <mpvinod625@gmail.com>
2022-07-29 13:34:07 +0530

    preparing to update 0.4.5 in pip

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+## sudo apt-get install python3.8-venv
+## sudo -H pip3 install build
+## sudo -H pip3 install twine
 ## python3 -m build
 ## python3 -m twine upload --repository pypi dist/*
 
@@ -6,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kavyanarthaki',
-    version='0.4.0',
+    version='0.4.5',
     license='MIT',
     author="Prof. Achuthsankar S Nair, Vinod M P",
     author_email='sankar.achuth@gmail.com, mpvinod625@gmail.com',
@@ -14,7 +17,7 @@ setup(
     include_package_data=True,
     package_data={
         "": ["*.txt"],
-        "kavyanarthaki": ["data/*.csv"],
+        "kavyanarthaki": ["data/*.csv","data/*.matrix"],
     },
     package_dir={'': 'src'},
     url='https://github.com/dcbfoss/vritham',
